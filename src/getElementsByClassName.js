97dc98a97f963ec7649d74cee141abe787c732b2
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-08-05 14:05:11 -0700

    Pass non-recursive case

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  debugger;
+	let node = [];
+  //go into document body and check for class
+  let children = document.body.childNodes;
+  if(document.body.classList.contains(className)) {
+    node.push(document.body);
+  }
+  _.each(children, function(ele) {
+    if(ele instanceof Element && ele.children.length <= 0 && ele.classList.contains(className)) {
+      node.push(ele);
+    } //else {
+    //   if(ele instanceof Element && ele.children.length > 0) {
+    //     return _.each(ele, getElementsByClassName());
+    //   }
+    //}
+    }); 			
+  //if multiple class elements, go down one path
+  		//push path to node if class
+  //return to fork and go down other path
+  		//return getElementsByclass
+	//         1
+	//        / \
+	//     [ 2   3 ]
+  //      /\    \
+  //   [ 4  5 ] [6]
+  //            / \ 
+  //         [ 7   8 ]
+  return node;
 };
+
+
+// You should use document.body, 
+// element.childNodes, 
+// and element.classList
